07a4e5c010385def987733dc633d10f0e8a3334e
Hristo <hristobak@gmail.com>
2017-03-28 20:23:25 +0200

    Additional features added

---
+++ BankWebsite/BankWebsite/Controllers/RedirectNotSignedIn.cs
@@ -0,0 +1,26 @@
+﻿using BankWebsite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Filters;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace BankWebsite.Controllers
+{
+    public class RedirectNotSignedIn : System.Web.Mvc.ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var user = HttpContext.Current.Session["userCredentionals"] as User;
+            // The action filter logic.
+            if (user == null || user.IsDisabled)
+            {
+                filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new { controller = "Home", action = "Index" }));
+
+            }
+
+        }
+    }
+}
---
+++ BankWebsite/Controllers/CustomerController.cs
@@ -0,0 +1,52 @@
+﻿using BankWebsite.Models;
+using SoSimpleDb;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BankWebsite.Controllers
+{
+    
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [RedirectNotSignedIn]
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public ActionResult UserActivation()
+        {
+
+            int userId = Convert.ToInt32(this.Request.QueryString["id"]);
+            string userToken = this.Request.QueryString["token"];
+            var user = SoSimpleDb<User>.Instance.Select(userId);
+            bool isUserAuthenticate;
+            if (user != null)
+            {
+                isUserAuthenticate = user.Token == userToken;
+                if (isUserAuthenticate)
+                {
+                    user.IsDisabled = false;
+                    SoSimpleDb<User>.Instance.Update(user);
+                    ViewBag.ActivationMessage = "Your account is activated!";
+                }
+                else
+                {
+                    ViewBag.ActivationMessage = "Your token sucks!";
+                }
+            }
+             
+
+            return View();
+        }
+    }
+}
